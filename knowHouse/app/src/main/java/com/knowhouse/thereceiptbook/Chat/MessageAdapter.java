591dc58ce72a157e48751166fbbc21118d80b13d
kelvin-tech <41630916+kelvin-tech@users.noreply.github.com>
2020-04-16 16:05:37 +0000

    update with correct package id

---
+++ knowHouse/app/src/main/java/com/knowhouse/thereceiptbook/Chat/MessageAdapter.java
@@ -0,0 +1,80 @@
+package com.knowhouse.thereceiptbook.Chat;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.knowhouse.thereceiptbook.R;
+import com.stfalcon.frescoimageviewer.ImageViewer;
+
+import java.util.ArrayList;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    ArrayList<MessageObject> messageList;
+
+    public MessageAdapter(ArrayList<MessageObject> messageList){
+        this.messageList = messageList;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, null, false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutView.setLayoutParams(lp);
+
+        MessageViewHolder rcv = new MessageViewHolder(layoutView);
+        return rcv;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MessageViewHolder holder, final int position) {
+        holder.mMessage.setText(messageList.get(position).getMessage());
+        //holder.mSender.setText(messageList.get(position).getSenderId());
+        holder.mSender.setText("");
+
+
+        if(messageList.get(holder.getAdapterPosition()).getMediaUrlList().isEmpty())
+            holder.mViewMedia.setVisibility(View.GONE);
+
+        holder.mViewMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ImageViewer.Builder(v.getContext(), messageList.get(holder.getAdapterPosition()).getMediaUrlList())
+                        .setStartPosition(0)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+
+
+
+
+    class MessageViewHolder extends RecyclerView.ViewHolder{
+        TextView    mMessage,
+                    mSender;
+        Button mViewMedia;
+        LinearLayout mLayout;
+        MessageViewHolder(View view){
+            super(view);
+            mLayout = view.findViewById(R.id.layout);
+            //mMessage.setLayoutParams(new RecyclerView.LayoutParams(32,32));
+
+            mMessage = view.findViewById(R.id.message);
+            mSender = view.findViewById(R.id.sender);
+            mViewMedia = view.findViewById(R.id.viewMedia);
+        }
+    }
+}
